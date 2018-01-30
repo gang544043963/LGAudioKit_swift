40716b544c9281562ab96d38575f55120283f39f
ligang <ligang@mail.jj.cn>
2018-01-30 14:04:50 +0800

    no message

--- LGAudioKit_swift/LGAudioKit_swift/ViewController.swift
+++ LGAudioKit_swift/LGAudioKit_swift/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	public var dataArray: NSMutableArray = []
+	public var tableView: UITableView!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -27,10 +30,11 @@ class ViewController: UIViewController {
 		let screenW = self.view.bounds.width
 		let screemH = self.view.bounds.height
 		let tableView = UITableView.init(frame: CGRect.init(x: 10, y: 20, width: screenW - 20, height: screemH - 70), style: .plain)
-		tableView.backgroundColor = .yellow
+		tableView.backgroundColor = .white
 		tableView.delegate = self
 		tableView.dataSource = self
 		self.view.addSubview(tableView)
+		self.tableView = tableView
 	}
 	
 	@objc func initButton() {
@@ -39,8 +43,12 @@ class ViewController: UIViewController {
 		let button = UIButton(type: .custom)
 		button.frame = CGRect(x: 10, y: screemH - 40, width: screenW - 20, height: 30)
 		button.setTitleColor(.blue, for: .selected)
-		button.setTitle("w3rq4q", for: .normal)
-		button.addTarget(self, action: #selector(btnTouched) , for: .touchUpInside)
+		button.setTitle("按住录音", for: .normal)
+		button.addTarget(self, action: #selector(startRecordVoice) , for: .touchDown)
+		button.addTarget(self, action: #selector(cancelRecordVoice) , for: .touchUpOutside)
+		button.addTarget(self, action: #selector(confirmRecordVoice) , for: .touchUpInside)
+		button.addTarget(self, action: #selector(updateCancelRecordVoice) , for: .touchDragExit)
+		button.addTarget(self, action: #selector(updateContinueRecordVoice) , for: .touchDragEnter)
 		button.layer.borderColor = UIColor.black.cgColor
 		button.layer.borderWidth = 1
 		button.layer.cornerRadius = 4
@@ -48,7 +56,30 @@ class ViewController: UIViewController {
 		self.view.addSubview(button)
 	}
 	
-	@objc func btnTouched() {
+	@objc func startRecordVoice() {
+		print("startRecordVoice...")
+	}
+	
+	@objc func cancelRecordVoice() {
+		print("cancelRecordVoice...")
+	}
+	
+	@objc func confirmRecordVoice() {
+		print("confirmRecordVoice...")
+		let soundModel = [
+			"soundFilePath": "",
+			"seconds": 5
+			] as [String : Any]
+		self.dataArray.addObjects(from: [soundModel])
+		self.tableView.reloadData()
+	}
+	
+	@objc func updateCancelRecordVoice() {
+		print("updateCancelRecordVoice...")
+	}
+	
+	@objc func updateContinueRecordVoice() {
+		print("updateContinueRecordVoice...")
 	}
 }
 
@@ -58,22 +89,32 @@ extension ViewController: UITableViewDelegate {
 
 extension ViewController: UITableViewDataSource {
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return 5
+		return self.dataArray.count
 	}
 	
 	func numberOfSections(in tableView: UITableView) -> Int {
-		return 2
+		return 1
 	}
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = UITableViewCell.init(style: .`default`, reuseIdentifier: nil)
+		let data: NSDictionary = self.dataArray[indexPath.row] as! NSDictionary
+		cell.textLabel?.text = String(describing: data["seconds"])
+		cell.backgroundColor = .purple
 		return cell
 	}
 	
 	func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-		let headerView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: 200, height: 30))
-		headerView.backgroundColor = UIColor.lightGray
-		return headerView
+		let textView = UITextView.init(frame: CGRect.init(x: 0, y: 60, width: self.view.frame.size.width, height: 100))
+		textView.backgroundColor = UIColor.red
+		textView.text = "欢迎使用本框架\n\n如果在使用过程中遇到问题请及时提issue\n博客:http://blog.csdn.net/gang544043963";
+		textView.textAlignment = NSTextAlignment.center
+		textView.font = UIFont.systemFont(ofSize: 17)
+		return textView
+	}
+	
+	func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+		return 100
 	}
 }
 
